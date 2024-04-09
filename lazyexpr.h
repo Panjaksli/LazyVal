71c91c067b75b66392656ac58e5fcd2a9fd3399a
Panjaksli <82727531+Panjaksli@users.noreply.github.com>
2024-04-09 14:02:57 +0200

    Unified to N-ary expression class
    Pretty much just simplified the code to have one master class with parameter pack, Unary, Binary and Ternary expressions are just specializations.
    Half the length, twice the fun. It is easier to overload operator= in your own class now.

--- lazyexpr.h
+++ lazyexpr.h
@@ -1,99 +1,67 @@
 #pragma once
 #include <type_traits>
 #include <cmath>
+#include <tuple>
 #include "lazyops.h"
 namespace Lazy {
 
-	template<typename T>
-	using NotInit = std::negation<std::is_same<T, std::initializer_list<typename T::value_type>>>;
-	template <typename T>
-	using IfScalar = std::is_scalar<typename std::remove_reference<T>::type>;
+	template <typename Op, typename ...Ts>
+	struct NaryExpr {
 
-	template <typename T>
-	inline auto index_wrapper(const T& val, size_t i) {
-		if constexpr(IfScalar<T>::value)
-			return val;
-		else return val[i];
-	}
+		template<typename T>
+		using NotInit = std::negation<std::is_same<T, std::initializer_list<typename T::value_type>>>;
+		template <typename T>
+		using IfScalar = std::is_scalar<typename std::remove_reference<T>::type>;
 
-	template <typename T>
-	inline size_t size_wrapper(const T& val) {
-		if constexpr(IfScalar<T>::value)
-			return size_t(-1);
-		else return val.size();
-	}
+		std::tuple<Ts...> args;
+		const Op op; // = Op();
+		// Uncomment if you want... There is really no need
+		//NaryExpr(Ts && ... _args) : args(std::forward<Ts>(_args)...) {}
+		NaryExpr(Op&& op, Ts && ... _args) : args(std::forward<Ts>(_args)...), op(std::forward<Op>(op)) {}
 
-	template <typename T1, typename Op>
-	struct UnExpr {
-		const T1 fst;
-		const Op op;
-
-		UnExpr(T1&& fst, Op&& op = Op()) : fst(std::forward<T1>(fst)), op(std::forward<Op>(op)) {}
-
-		inline size_t size() const { return size_wrapper(fst); }
-
-		template <typename Tres, typename = std::enable_if_t<NotInit<Tres>::value>>
-		inline operator Tres() const { return eval<Tres>();}
-
-		template <typename Tres>
-		inline Tres eval() const {
-			size_t sz = size() == size_t(-1) ? 1 : size();
-			auto res = Tres(sz);
-			for(size_t i = 0; i < sz; i++) res[i] = operator[](i);
-			return res;
+		inline size_t size() const {
+			const auto call = [](Ts const& ... arg) { return std::min(size_wrapper(arg)...); };
+			return std::apply(call, args);
 		}
-
-		inline auto operator[](size_t i)const { return op(index_wrapper(fst, i)); }
-	};
-
-	template <typename T1, typename T2, typename Op>
-	struct BinExpr {
-		const T1 fst;
-		const T2 snd;
-		const Op op;
-
-		BinExpr(T1&& fst, T2&& snd, Op&& op = Op()) : fst(std::forward<T1>(fst)), snd(std::forward<T2>(snd)), op(std::forward<Op>(op)) {}
-
-		inline size_t size() const { return std::min(size_wrapper(fst), size_wrapper(snd)); }
 
 		template <typename Tres, typename = std::enable_if_t<NotInit<Tres>::value>>
 		inline operator Tres() const { return eval<Tres>(); }
 
 		template <typename Tres>
 		inline Tres eval() const {
-			size_t sz = size() == size_t(-1) ? 1 : size();
-			auto res = Tres(sz);
-			for(size_t i = 0; i < sz; i++) res[i] = operator[](i);
+			size_t res_size = size();
+			res_size = res_size == size_t(-1) ? 1 : res_size;
+			auto res = Tres(res_size);
+			for(size_t i = 0; i < res_size; i++)
+				res[i] = operator[](i);
 			return res;
 		}
 
-		inline auto operator[](size_t i)const { return op(index_wrapper(fst, i), index_wrapper(snd, i)); }
-
-	};
-	// Ternary is all you ever need (implemented manually rather than using tuples... might revisit someday)
-	template <typename T1, typename T2, typename T3, typename Op>
-	struct TerExpr {
-		const T1 fst;
-		const T2 snd;
-		const T3 thd;
-		const Op op;
-
-		TerExpr(T1&& fst, T2&& snd, T3&& thd, Op&& op = Op()) : fst(std::forward<T1>(fst)), snd(std::forward<T2>(snd)), thd(std::forward<T3>(thd)), op(std::forward<Op>(op)) {}
-
-		inline size_t size() const { return std::min(std::min(size_wrapper(fst), size_wrapper(snd)), size_wrapper(thd)); }
-
-		template <typename Tres, typename = std::enable_if_t<NotInit<Tres>::value>>
-		inline operator Tres() const { return eval<Tres>(); }
-
-		template <typename Tres>
-		inline Tres eval() const {
-			size_t sz = size() == size_t(-1) ? 1 : size();
-			auto res = Tres(sz);
-			for(size_t i = 0; i < sz; i++) res[i] = operator[](i);
-			return res;
+		inline auto operator[](size_t i)const {
+			const auto call = [this, i](Ts const& ... arg) { return op(index_wrapper(arg, i)...); };
+			return std::apply(call, args);
 		}
 
-		inline auto operator[](size_t i)const { return op(index_wrapper(fst, i), index_wrapper(snd, i), index_wrapper(thd, i)); }
+		template <typename T>
+		static auto index_wrapper(const T& val, size_t i) {
+			if constexpr(IfScalar<T>::value)
+				return val;
+			else return val[i];
+		}
+
+		template <typename T>
+		static size_t size_wrapper(const T& val) {
+			if constexpr(IfScalar<T>::value)
+				return size_t(-1);
+			else return val.size();
+		}
+
 	};
 
+	template <typename Op, typename T1>
+	using UnExpr = NaryExpr<Op, T1>;
+	template <typename Op, typename T1, typename T2>
+	using BinExpr = NaryExpr<Op, T1, T2>;
+	template <typename Op, typename T1, typename T2, typename T3>
+	using TerExpr = NaryExpr<Op, T1, T2, T3>;
 }
--- lazyval.h
+++ lazyval.h
@@ -4,105 +4,109 @@ namespace Lazy {
 	//Ternary operands
 	template<typename U1, typename U2, typename U3>
 	inline auto cond(U1&& a, U2&& b, U3&& c) {
-		return TerExpr<U1, U2, U3, OpCond>(std::forward<U1>(a), std::forward<U2>(b), std::forward<U3>(c));
+		return NaryExpr<OpCond, U1, U2, U3>(OpCond(), std::forward<U1>(a), std::forward<U2>(b), std::forward<U3>(c));
 	}
 	template<typename U1, typename U2, typename U3>
 	inline auto limit(U1&& a, U2&& b, U3&& c) {
-		return TerExpr<U1, U2, U3, OpLim>(std::forward<U1>(a), std::forward<U2>(b), std::forward<U3>(c));
+		return NaryExpr<OpLim, U1, U2, U3>(OpLim(), std::forward<U1>(a), std::forward<U2>(b), std::forward<U3>(c));
 	}
 	template<typename U1, typename U2, typename U3, typename TeOp>
 	inline auto map(U1&& a, U2&& b, U3&& c, TeOp&& op = TeOp()) {
-		return TerExpr<U1, U2, U3, TeOp>(std::forward<U1>(a), std::forward<U2>(b), std::forward<U3>(c), std::forward<TeOp>(op));
+		return NaryExpr<TeOp, U1, U2, U3>(std::forward<TeOp>(op), std::forward<U1>(a), std::forward<U2>(b), std::forward<U3>(c));
 	}
 	//Binary operands
 	template<typename U1, typename U2, typename BiOp>
 	inline auto map(U1&& a, U2&& b, BiOp&& op = BiOp()) {
-		return BinExpr<U1, U2, BiOp>(std::forward<U1>(a), std::forward<U2>(b), std::forward<BiOp>(op));
+		return NaryExpr<BiOp, U1, U2>(std::forward<BiOp>(op), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator+(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpAdd>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpAdd, U1, U2>(OpAdd(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator-(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpSub>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpSub, U1, U2>(OpSub(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator*(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpMul>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpMul, U1, U2>(OpMul(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator/(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpDiv>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpDiv, U1, U2>(OpDiv(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator==(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpEq>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpEq, U1, U2>(OpEq(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator!=(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpNeq>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpNeq, U1, U2>(OpNeq(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator>=(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpGte>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpGte, U1, U2>(OpGte(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator<=(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpLte>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpLte, U1, U2>(OpLte(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator<(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpLt>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpLt, U1, U2>(OpLt(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto operator>(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpGt>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpGt, U1, U2>(OpGt(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto min(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpMin>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpMin, U1, U2>(OpMin(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto max(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpMax>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpMax, U1, U2>(OpMax(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 	template<typename U1, typename U2>
 	inline auto pow(U1&& a, U2&& b) {
-		return BinExpr<U1, U2, OpPow>(std::forward<U1>(a), std::forward<U2>(b));
+		return NaryExpr<OpPow, U1, U2>(OpPow(), std::forward<U1>(a), std::forward<U2>(b));
 	}
 
 	// Unary operands
 	template<typename U1, typename UnOp>
 	inline auto map(U1&& a, UnOp&& op = UnOp()) {
-		return UnExpr<U1, UnOp>(std::forward<U1>(a), std::forward<UnOp>(op));
+		return NaryExpr<UnOp, U1>(std::forward<UnOp>(op), std::forward<U1>(a));
 	}
 	template<typename U1>
 	inline auto operator-(U1&& a) {
-		return UnExpr<U1, OpNeg>(std::forward<U1>(a));
+		return NaryExpr<OpNeg, U1>(OpNeg(),std::forward<U1>(a));
+	}
+	template<typename U1>
+	inline auto identity(U1&& a) {
+		return NaryExpr<OpId, U1>(OpId(), std::forward<U1>(a));
 	}
 	template<typename U1>
 	inline auto sqrt(U1&& a) {
-		return UnExpr<U1, OpSqrt>(std::forward<U1>(a));
+		return NaryExpr<OpSqrt, U1>(OpSqrt(), std::forward<U1>(a));
 	}
 	template<typename U1>
 	inline auto sqr(U1&& a) {
-		return UnExpr<U1, OpSqr>(std::forward<U1>(a));
+		return NaryExpr<OpSqr, U1>(OpSqr(), std::forward<U1>(a));
 	}
 	template<typename U1>
 	inline auto exp(U1&& a) {
-		return UnExpr<U1, OpExp>(std::forward<U1>(a));
+		return NaryExpr<OpExp, U1>(OpExp(), std::forward<U1>(a));
 	}
 	template<typename U1>
 	inline auto log(U1&& a) {
-		return UnExpr<U1, OpLog>(std::forward<U1>(a));
+		return NaryExpr<OpLog, U1>(OpLog(), std::forward<U1>(a));
 	}
 	template<typename U1>
 	inline auto log2(U1&& a) {
-		return UnExpr<U1, OpLog2>(std::forward<U1>(a));
+		return NaryExpr<OpLog2, U1>(OpLog2(), std::forward<U1>(a));
 	}
 	template<typename U1>
 	inline auto log10(U1&& a) {
-		return UnExpr<U1, OpLog10>(std::forward<U1>(a));
+		return NaryExpr<OpLog10, U1>(OpLog10(), std::forward<U1>(a));
 	}
 }
